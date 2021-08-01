a69fe2f8c23eb75cf92a2eaf9153e22da2f968bb
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-07-31 21:30:05 -0400

    added initial app file

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# from app.extensions import TODO
+# from app.models import TODO
+
+def create_app(config_object="app.settings"):
+
+    app = Flask(__name__.split(".")[0])
+    app.config.from_object(config_object)
+
+    return app
