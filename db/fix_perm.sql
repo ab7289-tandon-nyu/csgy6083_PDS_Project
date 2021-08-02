dbb763a25d5d2872b1409b02ec5b1ef874ef859d
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-08-02 14:24:55 -0400

    removed unneeded files

--- db/fix_perm.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER 'root'@'172.30.0.3' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.30.0.3';
-FLUSH PRIVILEGES;
-
-grant all on *.* to 'root'@'%';
-flush privileges;
