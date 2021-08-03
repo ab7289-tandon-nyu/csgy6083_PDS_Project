5904e196535c20a33d6fce52bdd8cd896aff2241
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-08-02 23:46:18 -0400

    begin refactor of DBManager class

--- app/db.py
+++ app/db.py
@@ -1,25 +1,35 @@
 # import os
 # from flask import Flask
-import mysql.connector
+# import mysql.connector
+
+# class DBManager:
+#     def __init__(
+#         self,
+#         database="ab_project",
+#         host="db",
+#         user="root",
+#         password_file=None,
+#         password="password",
+#     ):
+#         self.connection = mysql.connector.connect(
+#             user=user,
+#             password=password,
+#             host=host,  # name of the mysql service as set in the docker-compose file
+#             database=database,
+#             auth_plugin="mysql_native_password",
+#         )
+#         self.cursor = self.connection.cursor()
+
+#     def populate_db(self):
+#         pass
 
 
 class DBManager:
-    def __init__(
-        self,
-        database="ab_project",
-        host="db",
-        user="root",
-        password_file=None,
-        password="password",
-    ):
-        self.connection = mysql.connector.connect(
-            user=user,
-            password=password,
-            host=host,  # name of the mysql service as set in the docker-compose file
-            database=database,
-            auth_plugin="mysql_native_password",
-        )
-        self.cursor = self.connection.cursor()
+    def __init__(self):
+        """
+        Initializes the DB manager class. At init it retrieves the mysql
+        object from extensions.
+        """
+        from .extensions import mysql
 
-    def populate_db(self):
-        pass
+        self.mysql = mysql
