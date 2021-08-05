8ef5c95a8ed326b87c7605ebcb5b2e938ad51f46
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-08-05 18:19:06 -0400

    added user and role models

--- app/user/models.py
+++ app/user/models.py
@@ -1,5 +1,31 @@
-# TODO implement user model objects
+class User:
+    """DTO Object for the User model"""
 
-# TODO implement role model objects
+    def __init__(self, user_id, user_name, email, type="C"):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.email = email
+        self.type = type
 
-# TODO implement ab_user_role_mtom objects
+    def __repr__(self):
+        """Represents instance as a unique string"""
+        return f"<User({self.user_id} {self.user_name}>"
+
+
+class Role:
+    """DTO Object for the Role Model"""
+
+    def __init__(self, role_id, name, desc):
+        self.role_id = role_id
+        self.name = name
+        self.desc = desc
+
+    def __repr__(self):
+        """Represents instance as a unique string"""
+        return f"<Role({self.role_id} {self.name})>"
+
+
+class UserRole:
+    def __init__(self, user_id, role_id):
+        self.user_id = user_id
+        self.role_id = role_id
