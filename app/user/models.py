e071d3f5d110b96158bea3fbc2e6b87f72e41624
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-08-05 18:29:34 -0400

    added password convenience methods

--- app/user/models.py
+++ app/user/models.py
@@ -1,11 +1,27 @@
-class User:
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+class User(UserMixin):
     """DTO Object for the User model"""
 
-    def __init__(self, user_id, user_name, email, type="C"):
+    def __init__(self, user_id, user_name, email, type="C", password=None):
         self.user_id = user_id
         self.user_name = user_name
         self.email = email
         self.type = type
+        if password:
+            self.set_password(password)
+        else:
+            self.password = None
+
+    def set_password(self, password):
+        """Convenience method to generate hashed and salted password"""
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        """convenience method to check that the supplied password matches the hash"""
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         """Represents instance as a unique string"""
