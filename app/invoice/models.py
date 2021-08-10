ea37ae09b339ec9454fe8a9d787b360c155bd9ff
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-08-10 09:26:32 -0400

    added invoice and payment models

--- app/invoice/models.py
+++ app/invoice/models.py
@@ -1,3 +1,22 @@
+from typing import List, Optional
+
+
+class Payment:
+    def __init__(
+        self,
+        pay_date: str,
+        amount: float,
+        pay_type: str,
+        p_id: int = -1,
+        invoice_id: int = -1,
+    ):
+        self.p_id = p_id
+        self.pay_date = pay_date
+        self.amount = amount
+        self.pay_type = pay_type
+        self.invoice_id = invoice_id
+
+
 class Invoice:
     def __init__(
         self,
@@ -8,6 +27,7 @@ class Invoice:
         active: bool,
         invoice_id: int = -1,
         policy_id: int = -1,
+        payments: Optional[List[Payment]] = None,
     ):
         self.invoice_date = invoice_date
         self.invoice_amount = invoice_amount
@@ -16,3 +36,7 @@ class Invoice:
         self.active = active
         self.invoice_id = invoice_id
         self.policy_id = policy_id
+        if payments:
+            self.payments = payments
+        else:
+            self.payments = []
