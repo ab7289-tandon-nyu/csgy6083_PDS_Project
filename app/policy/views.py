207809f24e5d4fd868e63dcea924766978c770d0
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2021-08-12 15:52:29 -0400

    added basic details and styling to policy page

--- app/policy/views.py
+++ app/policy/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, abort, render_template
 from flask_login import current_user, login_required
 
+from app.invoice.managers import InvoiceManager
+
+# from app.invoice.models import Invoice
 from app.policy.managers import PolicyManager
 
 bp = Blueprint("policy", __name__)
@@ -11,8 +14,19 @@ bp = Blueprint("policy", __name__)
 def policy(policy_id: int):
     manager = PolicyManager()
     policy = manager.get_by_id(policy_id)
+
+    homes = None
+    autos = None
+    invoices = None
+
     if not policy:
         abort(404)
     elif policy.user_id is not current_user.user_id:
         abort(401)
-    return render_template("policy/policy.html", p=policy)
+
+    i_manager = InvoiceManager()
+    invoices = i_manager.get_by_policy(policy_id)
+
+    return render_template(
+        "policy/policy.html", p=policy, invoices=invoices, homes=homes, autos=autos
+    )
